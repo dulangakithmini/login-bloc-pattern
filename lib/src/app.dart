a36c6fcea27d50b30f335b87b87a8dd554a6051d
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-12 20:04:38 +0530

    Create LoginScreen class

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:login_bloc/src/screens/login_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Login',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Login'),
+        ),
+        body: LoginScreen(),
+      ),
+    );
   }
 }
