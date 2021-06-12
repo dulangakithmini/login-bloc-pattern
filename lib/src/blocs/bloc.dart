ffda8881c7f886ff8c8e21c19ed369f2c160b3e6
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-13 00:34:14 +0530

    Create 2 StreamControllers for email and password, inside Bloc class

--- lib/src/blocs/bloc.dart
+++ lib/src/blocs/bloc.dart
@@ -1,1 +1,7 @@
+import 'dart:async';
 
+class Bloc {
+  final StreamController<String> emailController = StreamController<String>();
+  final StreamController<String> passwordController =
+      StreamController<String>();
+}
