b34ac305a1b0a84f6af0bfb49c26e90ce07e46f4
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-13 01:21:19 +0530

    Create a StreamTransformer to validate password

--- lib/src/blocs/validation_mixin.dart
+++ lib/src/blocs/validation_mixin.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 class ValidationMixin {
+  /// StreamTransformer to validate email
   final StreamTransformer<String, String> emailValidator =
       StreamTransformer.fromHandlers(
           handleData: (String email, EventSink sink) {
@@ -8,6 +9,17 @@ class ValidationMixin {
       sink.add(email);
     } else {
       sink.addError('Enter a Valid Email');
+    }
+  });
+
+  /// StreamTransformer to validate password
+  final StreamTransformer<String, String> passwordValidator =
+      StreamTransformer.fromHandlers(
+          handleData: (String password, EventSink sink) {
+    if (password.length > 8) {
+      sink.add(password);
+    } else {
+      sink.addError('Password must at least have 8 characters!');
     }
   });
 }
