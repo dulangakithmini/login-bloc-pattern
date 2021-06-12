28e2049978db0a3e483e295b4bc7f42b74d1ae99
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-12 21:05:26 +0530

    Add keyboard type to email address field

--- lib/src/screens/login_screen.dart
+++ lib/src/screens/login_screen.dart
@@ -35,6 +35,7 @@ class LoginScreen extends StatelessWidget {
         labelText: 'Email',
         hintText: 'me@test.com',
       ),
+      keyboardType: TextInputType.emailAddress,
     );
   }
 }
