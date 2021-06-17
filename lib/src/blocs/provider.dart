8a9ebde6ef005ca2d0ba1f2b83398bb22e5ee506
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-18 01:36:50 +0530

    Create a Bloc instance inside the Provider class

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -1,6 +1,10 @@
 import "package:flutter/material.dart";
 
+import 'bloc.dart';
+
 class Provider extends InheritedWidget {
+  final bloc = Bloc();
+
   @override
   bool updateShouldNotify(covariant InheritedWidget oldWidget) {
     return true;
