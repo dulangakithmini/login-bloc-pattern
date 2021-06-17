ca75437b601cfd94e3a352cdc93db2254cdf254e
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-18 01:35:05 +0530

    Create Provider class extending InheritedWidget and create updateShouldNotify function

--- lib/src/blocs/provider.dart
+++ lib/src/blocs/provider.dart
@@ -1,1 +1,9 @@
+import "package:flutter/material.dart";
 
+class Provider extends InheritedWidget {
+  @override
+  bool updateShouldNotify(covariant InheritedWidget oldWidget) {
+    return true;
+    // throw UnimplementedError();
+  }
+}
