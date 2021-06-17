62320c2e016bba812491924502c42df6dd6936bd
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-18 04:21:18 +0530

    Wrap MaterialApp by the Provider

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:login_bloc/src/screens/login_screen.dart';
 
+import 'blocs/provider.dart';
+
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Login',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Login'),
+    /// Wrap MaterialApp by the Provider
+    return Provider(
+      child: MaterialApp(
+        title: 'Login',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Login'),
+          ),
+          body: LoginScreen(),
         ),
-        body: LoginScreen(),
       ),
     );
+
+    // return MaterialApp(
+    //   title: 'Login',
+    //   home: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Login'),
+    //     ),
+    //     body: LoginScreen(),
+    //   ),
+    // );
   }
 }
