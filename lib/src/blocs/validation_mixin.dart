179d03bd133e612fe5955feda6db33e10dacdcdb
Dulanga Kithmini <dulangakithmini@gmail.com>
2021-06-20 18:17:57 +0530

    Set password validation length to 8 characters

--- lib/src/blocs/validation_mixin.dart
+++ lib/src/blocs/validation_mixin.dart
@@ -16,7 +16,7 @@ mixin ValidationMixin {
   final StreamTransformer<String, String> passwordValidator =
       StreamTransformer.fromHandlers(
           handleData: (String password, EventSink sink) {
-    if (password.length > 8) {
+    if (password.length > 7) {
       sink.add(password);
     } else {
       sink.addError('Password must at least have 8 characters!');
